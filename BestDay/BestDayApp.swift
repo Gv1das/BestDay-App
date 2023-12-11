afbfc6d70fa0832f2a8eef7fc6c4ff82399497e8
Gv1das <124292804+Gv1das@users.noreply.github.com>
2023-12-11 11:17:08 +0000

    Initial Commit

---
+++ BestDay/BestDayApp.swift
@@ -0,0 +1,17 @@
+//
+//  BestDayApp.swift
+//  BestDay
+//
+//  Created by Gvidas Lekstutis on 11/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct BestDayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BestDay/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BestDay
+//
+//  Created by Gvidas Lekstutis on 11/12/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
